58ff360273478b41359563a2f5c1957aa51107e4
Balázs Kiss <dev@balazskiss.name>
2019-08-22 18:44:56 +0200

    no message

--- Package.swift
+++ Package.swift
@@ -7,11 +7,11 @@ let package = Package(
         .library(name: "MongoDriver", targets: ["MongoDriver"])
     ],
     dependencies: [
-        //.package(url: "https://github.com/vapor/fluent.git", from: "2.5.1"),
-        //.package(url: "https://github.com/OpenKitten/MongoKitten.git", from: "4.0.19")
+        .package(url: "https://github.com/vapor/fluent.git", from: "2.5.1"),
+        .package(url: "https://github.com/OpenKitten/MongoKitten.git", from: "4.1.4-swift5")
     ],
     targets: [
-        .target(name: "MongoDriver", dependencies: []),
-        .testTarget(name: "MongoDriverTests", dependencies: [])
+        .target(name: "MongoDriver", dependencies: ["Fluent", "MongoKitten"]),
+        .testTarget(name: "MongoDriverTests", dependencies: ["MongoDriver", "FluentTester"])
     ]
 )
